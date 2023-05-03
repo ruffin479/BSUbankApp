a42937731c99c67af1a63e9afd7012044cb3e47e
00Phillipn <phillip00nguyen@gmail.com>
2023-05-02 20:48:55 -0400

    first commit

--- app.js
+++ app.js
@@ -78,4 +78,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
 module.exports = app;
